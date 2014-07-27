1ec4da3421e7137c75bccf10be891a0a1d193110
Gautam Akiwate <gautam.akiwate@gmail.com>
2014-07-26 22:53:17 -0700

    Added Usage for get*Commands to test_titanic.py

--- test_titanic.py
+++ test_titanic.py
@@ -4,11 +4,13 @@ buildername = 'Windows XP 32-bit mozilla-inbound pgo talos svgr'
 branch = 'mozilla-inbound'
 delta = 30
 
-# NOTE: This API might take long to run. This usually takes around a minute to run
-revList, buildList = titanic.runAnalysis(branch, buildername, 'ceff7d54080f', delta)
+# NOTE: This API might take long to run.
+# Usually takes around a minute to run, may take longer
+revList, buildList = titanic.runAnalysis(
+    branch, buildername, 'ceff7d54080f', delta)
 
-# for buildRev in buildList:
-#     print titanic.getBuildCommands(branch, buildername, buildRev)
-# 
-# for rev in revList:
-#     print titanic.getTriggerCommands(branch, buildername, rev)
+for buildRev in buildList:
+    print titanic.getBuildCommands(branch, buildername, buildRev)
+
+for rev in revList:
+    print titanic.getTriggerCommands(branch, buildername, rev)
